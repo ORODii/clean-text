9431a5c8e0654380facc1486860aa7775b8e1109
Andrés Orozco <aorozco@simetrical.com>
2018-10-17 18:59:45 -0500

    Se deja de utilizar gulp para utilizar webpack, se realizan las configuraciones necesarias

--- src/main.js
+++ src/main.js
@@ -1,25 +1,17 @@
-let cleanText;
-export default cleanText = function() {
-
-  const emailReg = new RegExp('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', 'g');
-
-  const phoneReg = new RegExp('/(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?/', 'g');
-
-  function checkEmailExist(text) {
-    return emailReg.test(text);
+class CleanText {
+  constructor() {
+    this.setup();
   }
 
-  function checkPhoneExist(text) {
-    return phoneReg.test(text);
+  setup() {
+    this.emailReg = new RegExp('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', 'g');
+
+    // this.phoneReg = new RegExp('/(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?/', 'g');
   }
 
-  function checkEmailOrPhoneExist(text) {
-    return checkEmailExist(text) || checkPhoneExist(text);
+  checkEmailExist(text) {
+    return this.emailReg.test(text);
   }
-};
-
-if (typeof window !== 'undefined') {
-  window.cleanText = cleanText;
-} else {
-  logStr('CleanText is a frontend module!');
 }
+
+module.exports = CleanText;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const pkg = require('./package.json');
+const path = require('path');
+const webpack = require('webpack');
+
+const banner = `clean-text.js v${pkg.version}
+https://github.com/ORODii/clean-text
+Licensed ISC © Andrés Orozco`;
+
+module.exports = (env, options) => {
+  return {
+    entry: './src/main.js',
+    output: {
+      filename: options.mode === 'development' ? 'clean-text.js' : 'clean-text.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'CleanText',
+        libraryTarget: 'umd'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new webpack.BannerPlugin({banner})
+    ]
+  };
+}
+
